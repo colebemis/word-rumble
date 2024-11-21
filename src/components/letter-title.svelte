81482b569e009bf2ec36db9fef053b4df9e27a63
Cole Bemis <colebemis@github.com>
2024-11-21 11:16:17 -0800

    Fix transition

--- src/components/letter-title.svelte
+++ src/components/letter-title.svelte
@@ -21,7 +21,7 @@
     // https://stackoverflow.com/questions/3885018/active-pseudo-class-doesnt-work-in-mobile-safari
   }}
   class={clsx(
-    "aspect-square rounded-lg border-2 [container-type:size] transition-[transform,color,background-color] will-change-transform active:scale-95 relative border-b-4",
+    "aspect-square rounded-lg border-2 [container-type:size] transition-[transform,color,background-color,border-color] will-change-transform active:scale-95 relative border-b-4",
     "focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-accent-11",
     selected
       ? "border-accent-8 text-accent-12 bg-accent-3 hover:bg-accent-4 active:bg-accent-5"
